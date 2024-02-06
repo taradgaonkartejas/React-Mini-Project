d219ebb674995d41f04102b62a85a7218ee60327
taradgaonkartejas <taradgaonkartejas@gmail.com>
2024-02-07 00:52:26 +0530

    video 10

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,6 +15,14 @@ export default function TextForm(props) {
         var lc=text.toLowerCase();
         setText(lc);
     }
+    const toCamelCase=()=>{
+        var lc=text.toLowerCase().split(' ').map(w=>w.charAt(0).toUpperCase()+ w.slice(1)).join(' ');
+        setText(lc);
+    }
+    const clearText=()=>{
+        var lc='';
+        setText(lc);
+    }
     return (
         <>
         <h1>{props.heading}</h1>
@@ -22,7 +30,9 @@ export default function TextForm(props) {
                 <textarea className="form-control" value={text} onChange={handleChange} id="TextArea" rows={6}></textarea>
             </div>
             <button type="button" className="btn btn-primary me-md-2" onClick={toUpperCase}>TO UPPERCASE</button>
-            <button type="button" className="btn btn-primary " onClick={toLowerCase}>to lowercase</button>
+            <button type="button" className="btn btn-primary me-md-2" onClick={toLowerCase}>to lowercase</button>
+            <button type="button" className="btn btn-primary me-md-2" onClick={toCamelCase}>to CamelCase</button>
+            <button type="button" className="btn btn-secondary " onClick={clearText}>Clear</button>
             <div className="container my-3">
                 <h2>Your Text Summery</h2>
             <p>{text.split(' ').length} words and {text.length} charactor</p>
